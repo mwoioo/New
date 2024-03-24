a67c43abef7d10a7ab3d249e230a7c1a091ebd52
mwoioo <164720641+mwoioo@users.noreply.github.com>
2024-03-25 02:00:41 +0530

    Update vctools.py

--- VIPMUSIC/plugins/tools/vctools.py
+++ VIPMUSIC/plugins/tools/vctools.py
@@ -7,36 +7,12 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 # vc on
 @app.on_message(filters.video_chat_started)
 async def brah(_, msg):
-    await msg.reply("**😍ᴠɪᴅᴇᴏ ᴄʜᴀᴛ sᴛᴀʀᴛᴇᴅ🥳**")
+    await msg.reply("**ᴠɪᴅᴇᴏ ᴄʜᴀᴛ sᴛᴀʀᴛᴇᴅ**")
 
 # vc off
 @app.on_message(filters.video_chat_ended)
 async def brah2(_, msg):
-    await msg.reply("**😕ᴠɪᴅᴇᴏ ᴄʜᴀᴛ ᴇɴᴅᴇᴅ💔**")
-
-# invite members on vc
-@app.on_message(filters.video_chat_members_invited)
-async def brah3(app: app, message: Message):
-    text = f"➻ {message.from_user.mention}\n\n**๏ ɪɴᴠɪᴛɪɴɢ ɪɴ ᴠᴄ ᴛᴏ :**\n\n**➻ **"
-    x = 0
-    for user in message.video_chat_members_invited.users:
-        try:
-            text += f"[{user.first_name}](tg://user?id={user.id}) "
-            x += 1
-        except Exception:
-            pass
-
-    try:
-        invite_link = await app.export_chat_invite_link(message.chat.id)
-        add_link = f"https://t.me/{app.username}?startgroup=true"
-        reply_text = f"{text} 🤭🤭"
-
-        await message.reply(reply_text, reply_markup=InlineKeyboardMarkup([
-            [InlineKeyboardButton(text= "๏ ᴊᴏɪɴ ᴠᴄ ๏", url=add_link)],
-        ]))
-    except Exception as e:
-        print(f"Error: {e}")
-
+    await msg.reply("**ᴠɪᴅᴇᴏ ᴄʜᴀᴛ ᴇɴᴅᴇᴅ**")
 
 ####
 
